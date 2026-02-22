8a1826ccd2997771f6765eae6c7dab8c32340be2
isaacwkm <61212700+isaacwkm@users.noreply.github.com>
2026-02-21 18:06:46 -0800

    inputs bound and added

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -16,12 +16,26 @@ public class GameInput
     private InputFrame player1Input;
     private InputFrame player2Input;
 
+    public struct FighterInput
+    {
+        public int MoveX; // -1, 0, 1 : left, neutral, right
+        public int MoveY; // -1, 0, 1 : down, neutral, up
+
+        public bool Light;
+        public bool Medium;
+        public bool Heavy;
+    }
+
     public void CaptureInput()
     {
         // Player 1
         player1Input.left = Input.GetKey(KeyCode.A);
         player1Input.right = Input.GetKey(KeyCode.D);
         player1Input.jump = Input.GetKey(KeyCode.Space);
+
+        player1Input.punchLight = Input.GetKey(KeyCode.J);
+        player1Input.punchMedium = Input.GetKey(KeyCode.K);
+        player1Input.punchHeavy = Input.GetKey(KeyCode.L);
 
         // Player 2 (example bindings)
         player2Input.left = Input.GetKey(KeyCode.LeftArrow);
