ba0216fbf9b2638cdf7e9d236cb8bc20d0f94508
isaacwkm <61212700+isaacwkm@users.noreply.github.com>
2026-02-26 21:10:37 -0800

    created sub-state machines for distinction between grounded and airborne

--- Assets/RoundstartOptions.cs
+++ Assets/RoundstartOptions.cs
@@ -1,16 +1,15 @@
 using UnityEngine;
 
+// Just some idea that popped in my head that probably can't be implemented until way later but...
+// Each character would have unique roundstart actions they can take to execute within the first
+// few frames of a round. These would be tied to an Rock-paper-scissors interaction where the player
+// can choose between engaging in low risk or high risk options.
+
 public class RoundstartOptions : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    void Start() { }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    void Update() { }
 }
